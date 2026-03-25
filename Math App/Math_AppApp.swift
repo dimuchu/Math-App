6725c114c96e132b05516183a589b7fef004e819
Dimuchu <udovikdmitry2001@gmail.com>
2026-03-25 06:55:40 +0100

    Initial Commit

---
+++ Math App/Math_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Math_AppApp.swift
+//  Math App
+//
+//  Created by Дмитрий on 25.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct Math_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
